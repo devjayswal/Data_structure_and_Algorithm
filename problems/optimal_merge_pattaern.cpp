c16f151467d326dcc8c312cbad1ab988f460c191
dev jayswal <rdssjayswal@gmail.com>
2023-08-09 03:47:17 +0000

    this is based on greedy approach its solution of problem optimal merge pattern

---
+++ problems/optimal_merge_pattaern.cpp
@@ -0,0 +1,37 @@
+/*you are given n files with their  computation times in an araay 
+operation:- choose/take  any two files , add their computation times and append it to the list of computation time 
+cost= sum of computation*/
+
+#include<bits/stdc++.h>
+using namespace std;
+#define int long long
+
+signed main(){
+    cout << "Enter the number of files : ";
+    int n;
+    cin >> n;
+    vector<int> a(n);
+
+    cout << "Enter the computation time of each file : ";
+    for(int i=0; i<n; i++){
+        cin >> a[i];
+    }
+
+    priority_queue<int, vector<int>, greater<int>> minHeap;
+    for(int i=0; i<n; i++){
+        minHeap.push(a[i]);
+    }
+    int ans = 0;
+    while(minHeap.size() > 1){
+        int e1 = minHeap.top();
+        minHeap.pop();
+        int e2 = minHeap.top();
+        minHeap.pop();
+        ans += e1 + e2;
+        minHeap.push(e1 + e2);
+    }
+    cout << "Minimum cost of merging is : ";
+    cout << ans << endl;
+    return 0;
+
+}
